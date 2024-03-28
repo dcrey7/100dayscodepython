33134ac04e58c463a5477e5c540e484accef357e
abhishek thomas <abhishek01789@gmail.com>
2024-03-28 23:35:13 +0530

    mail

--- day16_snakegame/scoreboard.py
+++ day16_snakegame/scoreboard.py
@@ -8,14 +8,25 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open(r"day16_snakegame/data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
-        self.goto(0, 260)
+        self.goto(0, 270)
         self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
 
     def game_over(self):
         self.pencolor("red")
@@ -24,5 +35,7 @@ class Scoreboard(Turtle):
 
     def increase_score(self):
         self.score += 1
-        self.clear()
+         
         self.update_scoreboard()
+
+
