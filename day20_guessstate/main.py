e815b689156055e50af5bca8501dd9d7de866488
abhishek thomas <abhishek01789@gmail.com>
2024-04-02 20:30:41 +0530

    day20_usastatev1

--- day20_guessstate/main.py
+++ day20_guessstate/main.py
@@ -3,11 +3,11 @@ import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
-image = "blank_states_img.gif"
+image = r"day20_guessstate/blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-data = pandas.read_csv("50_states.csv")
+data = pandas.read_csv(r"day20_guessstate/50_states.csv")
 all_states = data.state.to_list()
 guessed_states = []
 
