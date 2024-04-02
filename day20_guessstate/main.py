7e6302d22c7c8de2cc3d51b26220587e8bff8671
abhishek thomas <abhishek01789@gmail.com>
2024-04-02 20:49:01 +0530

    statefinalday20

--- day20_guessstate/main.py
+++ day20_guessstate/main.py
@@ -20,7 +20,7 @@ while len(guessed_states) < 50:
             if state not in guessed_states:
                 missing_states.append(state)
         new_data = pandas.DataFrame(missing_states)
-        new_data.to_csv("states_to_learn.csv")
+        new_data.to_csv(r"day20_guessstate/states_to_learn.csv")
         break
     if answer_state in all_states:
         guessed_states.append(answer_state)
