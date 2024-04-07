f6f4873d65e57f732f78262d6120df1b5e62d25b
abhishek thomas <abhishek01789@gmail.com>
2024-04-07 23:25:12 +0530

    day24_passgen

---
+++ day24_passwordgen/main.py
@@ -0,0 +1,42 @@
+from tkinter import *
+
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
+password_list = []
+
+for char in range(nr_letters):
+  password_list.append(random.choice(letters))
+
+for char in range(nr_symbols):
+  password_list += random.choice(symbols)
+
+for char in range(nr_numbers):
+  password_list += random.choice(numbers)
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
+
+
+window=Tk()
+window.title("password manager")
+window.config(padx=20,pady=20)
+
+canvas=Canvas(height=200,width=200)
+logo_img=PhotoImage(file=r"day24_passwordgen/IMG-2699.png")
+canvas.create_image(100,100,image=logo_img)
+canvas.pack()
+
+window.mainloop()
