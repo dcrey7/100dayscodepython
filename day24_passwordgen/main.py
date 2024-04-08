4c0c0497e59875851ff977d4ea8fb0c178a671c5
abhishek thomas <abhishek01789@gmail.com>
2024-04-08 19:24:04 +0530

    day24_passgen_v2

--- day24_passwordgen/main.py
+++ day24_passwordgen/main.py
@@ -24,10 +24,10 @@ def save():
         email_entry.delete(0,END)
 
 
+#Password Generator Project
 
 
 def generate_password():
-#Password Generator Project
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
